67400bfbde07d0125f789125576e81d7fa1f1890
Nikhilesh <sankanikhilesh001@gmail.com>
2024-05-13 18:37:38 +0530

    this is a random message generator

---
+++ randommsg.js
@@ -0,0 +1,5 @@
+const message1=['good','bad','great','worst','awfull','nice','amazing'];
+let  num = Math.floor(Math.random()*7);
+console.log(`I love to have a ${message1[num]} sex`);
+console.log(`I love to have ${message1[num]} food`);
+console.log(`I love to kill ${message1[num]} people`);
